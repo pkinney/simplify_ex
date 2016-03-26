a1cb596c3cecd10cdc029196a896fa50e2db80d5
Powell Kinney <powell@vin.li>
2016-03-26 00:42:59 -0500

    published to hex

--- mix.exs
+++ mix.exs
@@ -5,6 +5,8 @@ defmodule Simplify.Mixfile do
     [app: :simplify,
      version: "0.1.0",
      elixir: "~> 1.2",
+     description: description,
+     package: package,
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      test_coverage: [tool: ExCoveralls],
@@ -21,6 +23,21 @@ defmodule Simplify.Mixfile do
       {:geo, "~> 1.0"},
       {:distance, "~> 0.1.0"},
       {:excoveralls, "~> 0.4", only: :test}
+    ]
+  end
+
+  defp description do
+    """
+    Implementation of the Ramer–Douglas–Peucker algorithm for reducing the number of points used to represent a curve.
+    """
+  end
+
+  defp package do
+    [
+      files: ["lib", "mix.exs", "README*"],
+      maintainers: ["Powell Kinney"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/pkinney/simplify_ex"}
     ]
   end
 end
