ec7d49876a6961dd679316c07bf43699274c7310
Powell Kinney <powell@vin.li>
2016-03-30 10:59:09 -0500

    Updating bench

--- bench/simplify_bench.exs
+++ bench/simplify_bench.exs
@@ -1,27 +1,41 @@
 defmodule SimplifyBench do
   use Benchfella
 
-  @ring Path.join([ "test", "fixtures", "large_linestring.wkt" ])
+  @long Path.join([ "test", "fixtures", "large_linestring.wkt" ])
     |> File.read!
     |> Geo.WKT.decode
 
-  bench "very low tolerance" do
-    Simplify.simplify(@ring, 0.00000001)
+  @short [{0, 0}, {0.01, 0}, {0.5, 0.01}, {0.7, 0}, {1, 0}, {1.999, 0.999}, {2,1}]
+
+  bench "long linestring - very low tolerance" do
+    Simplify.simplify(@long, 0.00000001)
   end
 
-  bench "normal tolerance" do
-    Simplify.simplify(@ring, 0.00001)
+  bench "long linestring - normal tolerance" do
+    Simplify.simplify(@long, 0.00001)
   end
 
-  bench "high tolerance" do
-    Simplify.simplify(@ring, 0.1)
+  bench "long linestring - high tolerance" do
+    Simplify.simplify(@long, 0.1)
   end
 
-  bench "very high tolerance" do
-    Simplify.simplify(@ring, 1)
+  bench "long linestring - very high tolerance" do
+    Simplify.simplify(@long, 1)
   end
 
-  bench "very very high tolerance" do
-    Simplify.simplify(@ring, 100)
+  bench "long linestring - very very high tolerance" do
+    Simplify.simplify(@long, 100)
+  end
+
+  bench "short linestring - high tolerance" do
+    Simplify.simplify(@short, 0.1)
+  end
+
+  bench "short linestring - normal tolerance" do
+    Simplify.simplify(@short, 0.009)
+  end
+
+  bench "short linestring - low tolerance" do
+    Simplify.simplify(@short, 0.00001)
   end
 end
