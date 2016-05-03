107265501cc681bb95d34534b02ab0b2dc7113ac
Powell Kinney <powell@vin.li>
2016-05-03 15:37:58 -0500

    Updated docs

---
+++ doc/dist/sidebar_items.js
@@ -0,0 +1,1 @@
+sidebarNodes={"exceptions":[],"extras":[{"id":"api-reference","title":"API Reference","headers":[]}],"modules":[{"id":"Simplify","title":"Simplify","functions":[{"id":"simplify/2","anchor":"simplify/2"}]}],"protocols":[]}
