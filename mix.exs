d01b6e7652bd7141346d321340cde05f456a6825
Powell Kinney <628952+pkinney@users.noreply.github.com>
2022-05-28 13:57:05 -0500

    Adds CI using Github Actions

--- mix.exs
+++ mix.exs
@@ -25,11 +25,12 @@ defmodule Simplify.Mixfile do
   defp deps do
     [
       {:geo, "~> 3.1"},
-      {:distance, "~> 0.2.1"},
-      {:ex_doc, ">= 0.0.0", only: :dev},
+      {:distance, "~> 1.0"},
       {:benchfella, "~> 0.3.0", only: :dev},
-      {:jason, "~> 1.1", only: :test},
+      {:jason, "~> 1.1", only: [:dev, :test]},
       {:excoveralls, "~> 0.4", only: :test},
+      {:earmark, "~> 1.2", only: :dev},
+      {:ex_doc, "~> 0.19", only: :dev},
       {:credo, "~> 1.6", only: :dev, runtime: false},
       {:dialyxir, "~> 1.0", only: :dev, runtime: false}
     ]
