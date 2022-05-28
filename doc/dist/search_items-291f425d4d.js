c4d5d1c65f5bd720c0a54a630bb9bf477afa212e
Powell Kinney <628952+pkinney@users.noreply.github.com>
2022-05-28 14:53:16 -0500

    Update docs for v2.0.0

--- doc/dist/search_items-291f425d4d.js
+++
@@ -1,1 +0,0 @@
-searchNodes=[{"ref":"Simplify.html","title":"Simplify","type":"module","doc":""},{"ref":"Simplify.html#simplify/2","title":"Simplify.simplify/2","type":"function","doc":""},{"ref":"Simplify.html#t:point/0","title":"Simplify.point/0","type":"type","doc":""}]
